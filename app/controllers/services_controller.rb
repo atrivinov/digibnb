a2f2841f300684a9cdd9d9f7391ac0ddf3db9828
mateo soto <mateo.sotoa@udea.edu.co>
2022-09-29 13:57:42 -0500

    Service Controller Actions

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,45 @@
 class ServicesController < ApplicationController
+  before_action set_service, only: %i[show edit update destroy]
+  def index
+    @services = Service.all
+  end
+
+  def show
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @service.update(service_params)
+    redirect_to service_path(@service)
+  end
+
+  def destroy
+    @service.destroy
+    redirect_to service_path
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:product_name, :description, :available, :price, :user_id)
+  end
+
+  def set_params
+    @service = Service.find(params[:id])
+  end
 end

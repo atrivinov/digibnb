96a3fb236b8e2bbde02a2511015db5403b70b76d
Linda Mejia <atenea1205@hotmail.com>
2022-09-29 20:57:01 -0500

    Bookings fixing bookings_params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -37,6 +37,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit()
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end

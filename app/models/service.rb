4bbe5beefe1dff762f0b8a9c3008fbf4992e0df8
Linda Mejia <atenea1205@hotmail.com>
2022-10-06 19:17:18 -0500

    Lógica del Search Terminada

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,7 @@ class Service < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
   has_one_attached :photo
+  validates :description, length: { in: 6..255 }
 
   before_save :set_defaults
 
@@ -10,4 +11,11 @@ class Service < ApplicationRecord
   def set_defaults
     self.available = true
   end
+
+  include PgSearch::Model
+  pg_search_scope :search_by_product_name_and_description,
+  against: [ :product_name, :description ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
